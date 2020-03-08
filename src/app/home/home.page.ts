71fbc166475ccc26e32113e17aff2cef0721a4b0
lud42.valentin@gmail.com <ludwig.valentin@briohr.com>
2020-03-08 16:28:47 +0800

    test pixijs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import * as PIXI from "pixi.js";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  @ViewChild("pixi", { static: true })
+  public pixiContainer: ElementRef;
+  private pixi: PIXI.Application;
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {}
 
+  ionViewDidEnter() {
+    this.pixi = new PIXI.Application({
+      width: 640,
+      height: 480,
+      backgroundColor: 0x112266
+    });
+    const aspectRatio = this.pixi.renderer.height / this.pixi.renderer.width;
+
+    // new PIXI
+
+    this.pixi.view.style.width = "100%";
+    this.pixi.view.style.height = `${aspectRatio * 100}%`;
+    this.pixiContainer.nativeElement.appendChild(this.pixi.view);
+
+    console.log(this.pixi);
+  }
 }
